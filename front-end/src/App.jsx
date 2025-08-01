a083e646a75696e0d6ddf535574a1a7076a3cbc5
pcfaria13 <pcfaria13@users.noreply.github.com>
2025-08-01 19:09:37 -0300

    ajuste

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -10,7 +10,7 @@ import axios from 'axios';
 axios.defaults.baseURL =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:3000/api'
-    : 'https://projeto-mercado-livre.onrender.com';
+    : 'https://projeto-mercado-livre.onrender.com/api';
 
 export const ContextoCarrinho = createContext(null);
 
